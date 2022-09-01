fe0d50b02fa7b3fe8bff96dc6e884822bf6c9c39
bradschillschool@gmail.com <bradschillschool@gmail.com>
2022-09-01 10:21:34 -0500

    m

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,12 +8,17 @@
 
 // Brads Connection
 
+const mongoose = require('mongoose');
+const is_production = process.env.NODE_ENV;
 
+const url = is_production ? 'mongodb+srv://b-alt-del:password12345@cluster0.c5whavk.mongodb.net/?retryWrites=true&w=majority' : 'mongodb://127.0.0.1:27017/ThePixeleers'
 
+  mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-
-
-
+module.exports = mongoose.connection;
 
 // Fatuuns Connection
 
