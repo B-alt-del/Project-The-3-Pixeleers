ef31a17aa18dbf0e6cfebcf5db81f46cc7c44e0e
bradschillschool@gmail.com <bradschillschool@gmail.com>
2022-09-01 10:09:31 -0500

    connection file

---
+++ server/config/connection.js
@@ -0,0 +1,28 @@
+
+// For now we will each have our own connection setup in here 
+// this way for testing we can connect to our own mongoDB and not have to keep changing it
+// whenever you do a pull, just comment out the other peoples connection and uncomment your own
+
+
+
+
+// Brads Connection
+
+
+
+
+
+
+
+
+// Fatuuns Connection
+
+
+
+
+
+
+
+
+
+//Lees Connection
