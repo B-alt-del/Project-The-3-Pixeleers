7725a591797070a9083046b79c56db589d383700
Fartuun Ali <fartuunali@Fartuuns-MacBook-Pro.local>
2022-09-01 14:43:52 -0500

    merging

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,7 +1,47 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+// import Box from '@mui/material/Box';
+import Container from '@material-ui/core/Container';
+
+// export default function BoxSx() {
+//   return (
+//     <Box
+//       sx={{
+//         width: 300,
+//         height: 300,
+//         backgroundColor: 'primary.dark',
+//         '&:hover': {
+//           backgroundColor: 'primary.main',
+//           opacity: [0.9, 0.8, 0.7],
+//         },
+//       }}
+//     />
+//   );
+// }
+
+
+
+
 function About(props) {
     return (
       <div>
-        <h1>About Page</h1>
+        <Container maxWidth="lg">
+        <h1  style={{ backgroundColor: '#cfe8fc'}}>
+          About Page
+         </h1>
+      </Container>
+        {/* <h1>About Page</h1> */}
+
+
+
+
+
+        <footer>
+          
+        <Avatar alt="Fartuun Ali" src="/static/images/avatar/1.jpg" />
+        <Avatar alt="Lee Boettcher" src="/static/images/avatar/2.jpg" />
+        <Avatar alt="Bradley Schill" src="/static/images/avatar/3.jpg" />  
+        </footer>
       </div>
     )
   }
