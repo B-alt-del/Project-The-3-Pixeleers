3448ae0c30733a398915cc02d01fecff1ffed657
Lizzle <leefboettcher@gmail.com>
2022-09-02 10:25:32 -0500

    early update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -58,7 +58,7 @@ export default function Login() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundImage: 'url(https://source.unsplash.com/random/art)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
