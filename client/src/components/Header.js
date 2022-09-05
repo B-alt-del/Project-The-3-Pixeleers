88eebd637f6d89f1649a65f264d0865b443dcd33
Lizzle <leefboettcher@gmail.com>
2022-09-05 16:31:48 -0500

    logo added to header-bar, menu created for navigation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,37 +1,71 @@
+import * as React from 'react';
 import { NavLink } from "react-router-dom";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Fade from '@mui/material/Fade'
+import MenuItem from '@mui/material/MenuItem';
+import logo from "./logos/water-dragon.png"
 // import { User } from "../../../server/models";
 
 function HeaderBar(props) {
-    return (
 
-    <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
-                    <h1 >{props.logo}</h1>
-                </Typography>
-                    <Box mr={2}><NavLink item xs to="/">Home</NavLink></Box> 
-                    <Box mr={2}><NavLink item xs to="/create">Create</NavLink></Box> 
-                    <Box mr={2}><NavLink item xs to="/login">Login</NavLink></Box> 
-                    <Box mr={2}><NavLink item xs to="/about">About</NavLink></Box> 
-            </Toolbar>
-        </AppBar>
-    </Box>
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="relative" style={{ background: '#2E3B55' }}>
+                <Toolbar>
+                    <Box
+                        component="img"
+                        sx={{ height: 54 }}
+                        alt="Logo"
+                        src={logo}
+                    />
+                    <Typography variant="h8" component="div" sx={{ flexGrow: 1 }}>
+                        <h1 >{props.logo}</h1>
+                    </Typography>
+                    <Button
+                        id="fade-button"
+                        size="large"
+                        aria-controls={open ? 'fade-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                        textColor="white"
+                    ><MenuIcon />
+                    </Button>
+                    <Menu
+                        id="fade-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'fade-button',
+                        }}
+                        TransitionComponent={Fade}
+                    >
+                        <MenuItem><NavLink to="/" >Home</NavLink></MenuItem>
+                        <MenuItem><NavLink to="/create">Create</NavLink></MenuItem>
+                        <MenuItem><NavLink to="/login">Login</NavLink></MenuItem>
+                        <MenuItem><NavLink to="/about">About</NavLink></MenuItem>
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 };
 
