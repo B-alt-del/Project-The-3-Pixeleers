a5d16e1dae26b3335615a2e5f07459550d45818f
Lizzle <leefboettcher@gmail.com>
2022-09-07 09:49:04 -0500

    create responsive + style

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -6,6 +6,7 @@ import { useMutation, useQuery } from '@apollo/client';
 import { ADD_PXL } from '../utilities/mutations'
 import PXLS from './PXLS'
 import { color } from "@mui/system";
+import { Grid } from '@mui/material';
 
 function Create(props) {
   const [PXLColorA, setPXLColorA] = useState("color")
@@ -16,7 +17,7 @@ function Create(props) {
   const [Color, setColor] = useState('rgb(0, 255, 0)')
   const [addPXL, { loading, data, error }] = useMutation(ADD_PXL, { variables: { name: PXLName, colorArr: PXLColorA } })
   const [count, setCount] = useState(1)
-  const dimensions = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  const dimensions = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
   const reset = () => {
     setCount(Math.random())
   }
@@ -33,7 +34,6 @@ function Create(props) {
   function changeColor(event) {
     let colorString = event.target.attributes.data.nodeValue
     setColor(colorString)
-    // console.log(colorString)
   }
 
   // function handleOpen //
@@ -42,12 +42,6 @@ function Create(props) {
     setOpen(true);
     console.log(tempPXL)
       ;
-  }
-
-  const handleClose = () => {
-    setOpen(false);
-    reset();
-    const { data, error } = addPXL();
   }
 
   const handleClose = () => {
@@ -75,29 +69,24 @@ function Create(props) {
     }}>
       <div id="maincontainer" style={{
         display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        width: "100%"
+        flexDirection: "column",
+        width: "100%",
       }}>
-        <div id="section1" style={{
+        {/* <div id="section1" style={{
           display: "flex",
-          flexDirection: "column",
           flexBasis: "100%",
-          flex: 1
         }}>
           <section id="templateArt">
             Links to pixel art templates
           </section>
-        </div>
+        </div> */}
         <div id="section2" style={{
           display: "flex",
           flexDirection: "column",
-          flexBasis: "100%",
-          flex: 1
         }}>
-          <div id='create'>
-            <h1>Create New PXL</h1>
-            <Button onClick={handleOpen}>Open modal</Button>
+          <div id='create' style={{justifyContent: "space-around"}}>
+            <h1>PXL Dashboard</h1>
+            <p><Button onClick={handleOpen} variant="outlined">Create New PXL!</Button></p>
             <Modal
               open={open}
               onClose={handleClose}
@@ -105,18 +94,14 @@ function Create(props) {
               aria-describedby="modal-modal-description"
               sx={{
                 display: "flex",
-                justifyContent: "center",
-                alignItems: "center"
+                justifyContent: "space-around",
               }}
             >
-              <Box sx={{
+              <Box id="boxOne" sx={{
                 display: "flex",
                 width: '90%',
                 height: '90%',
-                justifyContent: 'space-evenly',
                 alignItems: "center",
-                margin: "10%",
-                padding: "10%",
 
                 backgroundColor: 'rgba(23,189,211, 75%)',
                 border: "2px solid black",
@@ -125,14 +110,15 @@ function Create(props) {
                   display: "flex",
                   width: '20%',
                   height: '100%',
-                  justifyContent: 'space-between',
+                  // justifyContent: 'space-between',
                   alignItems: "center",
-                  margin: "10%",
-                  padding: "10%",
+                  // margin: "10%",
+                  // padding: "10%",
 
                   backgroundColor: 'rgba(0,135,153)',
                   border: "2px solid black",
                 }}>
+
                   <Box sx={{ width: '100%' }}>
                     <Stack spacing={1}>
                       {colorPallete.map((color, index) => (
@@ -141,29 +127,75 @@ function Create(props) {
                         </ListItem>))}
                     </Stack>
                   </Box>
+
                 </Box>
-                <div >
-                  <Button id="saveBtn" onClick={savePXL} > SAVE PROJECT </Button>
-                  <Button id="deleteBtn" > DELETE </Button>
-                </div>
+
                 <Canvas
                   width={canvasWidth}
                   height={canvasHeight}
                   Color={Color}
                   PXL={PXL}
                   setPXL={setPXL} />
+                <div id="modalButtons">
+                  <form >
+                    <h1>Canvas Dimensions</h1>
+
+                    {/* <input
+                    value={todo_text} //canvas width
+                    onChange={e => setTodoText(e.target.value)}
+                    type="number"
+                    placeholder="Enter your todo text" />
+                  <button>Submit</button> */}
+                    <div>
+                      <TextField
+                        id="widthInput"
+                        select
+                        label="Width"
+                        type="number"
+                        value={canvasWidth}
+                        onChange={e => setCanvasWidth(e.target.value)}
+                        helperText="Width"
+                      >
+                        {dimensions.map((option) => (
+                          <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                      <TextField
+                        id="heightInput"
+                        select
+                        label="Height"
+                        value={canvasHeight}
+                        onChange={setCanvasHeight}
+                        helperText="Height"
+                      >
+                        {dimensions.map((option) => (
+                          <option key={option.valueOf} value={option.valueOf}>
+                          </option>
+                        ))}
+                      </TextField>
+                    </div>
+
+                  </form>
+                  <Button id="saveBtn" onClick={savePXL} > SAVE PROJECT </Button>
+                  <Button id="closeBtn" onClick={handleClose}> CLOSE </Button>
+                  {/* <Button id="delBtn" onClick={handleClose}> CLOSE </Button> */}
+                </div>
 
               </Box>
             </Modal >
           </div >
           <div id="section3" style={{
             display: "flex",
-            flexDirection: "column",
-            flexBasis: "100%",
-            flex: 1
+            justifyContent: "space-evenly",
           }}>
-            Pixel Cards
-            <PXLS key={count} />
+            <section style={{
+              display: "flex"
+            }}>
+              <h3>Saved PXL Cards</h3>
+              <PXLS key={count} />
+            </section>
           </div>
         </div>
       </div>
@@ -172,3 +204,4 @@ function Create(props) {
 }
 
 export default Create;
+
